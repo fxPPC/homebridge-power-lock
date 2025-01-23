77ccdafb84d58aa3588df0b336aeb133388f78dd
Alice <68247743+fxPPC@users.noreply.github.com>
2025-01-23 00:42:55 -0600

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,9 @@
   "parserOptions": {
     "project": "./tsconfig.json"
   },
-  "plugins": ["@typescript-eslint"],
+  "plugins": [
+    "@typescript-eslint"
+  ],
   "extends": [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended"
