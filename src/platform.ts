7936336a16d7ac78ed62dca6862163fd3423b878
Alice <68247743+fxPPC@users.noreply.github.com>
2025-01-23 16:02:57 -0600

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -1,150 +1,104 @@
-import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+'use strict';
 
-import { ExamplePlatformAccessory } from './platformAccessory.js';
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
+const { PowerLockAccessory } = require('./powerlockAccessory');
 
-// This is only required when using Custom Services and Characteristics not support by HomeKit
-import { EveHomeKitTypes } from 'homebridge-lib/EveHomeKitTypes';
+class PowerLockPlatform {
+  constructor(log, config, api) {
+    this.log = log;
+    this.api = api;
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
-  public readonly Service: typeof Service;
-  public readonly Characteristic: typeof Characteristic;
+    if (!config) {
+      this.log.warn('No configuration found for PowerLockPlatform.');
+      return;
+    }
 
-  // this is used to track restored cached accessories
-  public readonly accessories: Map<string, PlatformAccessory> = new Map();
-  public readonly discoveredCacheUUIDs: string[] = [];
+    this.name = config.name || 'PowerLockPlatform';
+    this.logging = config.logging || 'normal';
+    this.locksConfig = config.locks || [];
 
-  // This is only required when using Custom Services and Characteristics not support by HomeKit
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public readonly CustomServices: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public readonly CustomCharacteristics: any;
+    // Keep references to the accessory handlers we create
+    this.accessories = [];
 
-  constructor(
-    public readonly log: Logging,
-    public readonly config: PlatformConfig,
-    public readonly api: API,
-  ) {
-    this.Service = api.hap.Service;
-    this.Characteristic = api.hap.Characteristic;
+    // Listen for shutdown to gracefully clean up
+    this.api.on('shutdown', () => {
+      this.logIf('debug', 'Shutting down...');
+      this.accessories.forEach((acc) => {
+        // Each "acc" in this.accessories might be a PowerLockAccessory object or a cached Accessory
+        // We only call .cleanup() if it's actually an instance of our handler
+        if (acc && acc.cleanup) {
+          acc.cleanup();
+        }
+      });
+    });
 
-    // This is only required when using Custom Services and Characteristics not support by HomeKit
-    this.CustomServices = new EveHomeKitTypes(this.api).Services;
-    this.CustomCharacteristics = new EveHomeKitTypes(this.api).Characteristics;
-
-    this.log.debug('Finished initializing platform:', this.config.name);
-
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
+    // When the API is ready, we can configure accessories
     this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.logIf('debug', 'didFinishLaunching: Starting to configure accessories');
+      this.configureAccessories();
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to set up event handlers for characteristics and update respective values.
-   */
-  configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
+  // Called by Homebridge to discover/update HomeKit accessories
+  configureAccessories() {
+    this.locksConfig.forEach((lockConfig) => {
+      if (!lockConfig.lockName) {
+        this.logIf('normal', 'Skipping lock with no lockName.');
+        return;
+      }
 
-    // add the restored accessory to the accessories cache, so we can track if it has already been registered
-    this.accessories.set(accessory.UUID, accessory);
+      const uuid = this.api.hap.uuid.generate(lockConfig.lockName);
+
+      // See if we already have an accessory for this UUID
+      let existingAccessory = this.accessories.find(a => a.UUID === uuid || a.accessory?.UUID === uuid);
+
+      if (existingAccessory && existingAccessory instanceof PowerLockAccessory) {
+        // Reconfigure existing one
+        this.logIf('normal', `Updating existing accessory ${lockConfig.lockName}`);
+        existingAccessory.configure(lockConfig);
+
+      } else if (existingAccessory && existingAccessory.displayName) {
+        // This might be a cached platformAccessory object from Homebridge
+        this.logIf('normal', `Wrapping cached accessory in PowerLockAccessory: ${lockConfig.lockName}`);
+        const powerLockAcc = new PowerLockAccessory(this, existingAccessory, lockConfig);
+        // Replace in the array
+        const index = this.accessories.indexOf(existingAccessory);
+        this.accessories[index] = powerLockAcc;
+
+      } else {
+        // Create a brand new accessory
+        this.logIf('normal', `Creating new accessory ${lockConfig.lockName}`);
+        const newAccessory = new this.api.platformAccessory(lockConfig.lockName, uuid);
+        const powerLockAcc = new PowerLockAccessory(this, newAccessory, lockConfig);
+
+        this.api.registerPlatformAccessories('homebridge-power-lock', 'PowerLockPlatform', [newAccessory]);
+        this.accessories.push(powerLockAcc);
+      }
+    });
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-      {
-        // This is an example of a device which uses a Custom Service
-        exampleUniqueId: 'IJKL',
-        exampleDisplayName: 'Backyard',
-        CustomService: 'AirPressureSensor',
-      },
-    ];
+  // Called by Homebridge at startup for each cached accessory
+  configureAccessory(accessory) {
+    this.logIf('debug', `Loading accessory from cache: ${accessory.displayName}`);
+    // Just store it; we’ll configure it properly in configureAccessories
+    this.accessories.push(accessory);
+  }
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+  // Helper logging method with levels: debug, normal, none
+  logIf(level, message, ...args) {
+    const levels = {
+      none: 0,
+      normal: 1,
+      debug: 2,
+    };
+    const currentLevel = levels[this.logging] ?? 1; // default is normal
+    const requiredLevel = levels[level] ?? 1;
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.get(uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
-
-      // push into discoveredCacheUUIDs
-      this.discoveredCacheUUIDs.push(uuid);
-    }
-
-    // you can also deal with accessories from the cache which are no longer present by removing them from Homebridge
-    // for example, if your plugin logs into a cloud account to retrieve a device list, and a user has previously removed a device
-    // from this cloud account, then this device will no longer be present in the device list but will still be in the Homebridge cache
-    for (const [uuid, accessory] of this.accessories) {
-      if (!this.discoveredCacheUUIDs.includes(uuid)) {
-        this.log.info('Removing existing accessory from cache:', accessory.displayName);
-        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+    if (requiredLevel <= currentLevel) {
+      this.log(message, ...args);
     }
   }
 }
+
+module.exports = {
+  PowerLockPlatform,
+};
