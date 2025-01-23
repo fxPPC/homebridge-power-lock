22dd62d85451321e9bda3fbc66c4aa583dcb4586
Alice <68247743+fxPPC@users.noreply.github.com>
2025-01-22 23:51:07 -0600

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,17 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-  {
-    ignores: ['dist/**'],
+{
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json"
   },
-  {
-    rules: {
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2, { 'SwitchCase': 0 }],
-      'linebreak-style': ['error', 'unix'],
-      'semi': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'dot-notation': 'error',
-      'eqeqeq': ['error', 'smart'],
-      'curly': ['error', 'all'],
-      'brace-style': ['error'],
-      'prefer-arrow-callback': 'warn',
-      'max-len': ['warn', 160],
-      'object-curly-spacing': ['error', 'always'],
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': ['error', { 'classes': false, 'enums': false }],
-      '@typescript-eslint/no-unused-vars': ['error', { 'caughtErrors': 'none' }],
-    },
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-    },
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+  "plugins": ["@typescript-eslint"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "no-console": "off"
+  }
+}
