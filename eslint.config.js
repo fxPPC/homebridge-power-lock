458152de57c1fc817d843e72dfe1c9242a9aef52
Alice <68247743+fxPPC@users.noreply.github.com>
2025-01-23 00:45:27 -0600

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
-{
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json"
-  },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "single"],
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "no-console": "off"
+// eslint.config.js
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'no-console': 'off'
+    }
   }
-}
+];
